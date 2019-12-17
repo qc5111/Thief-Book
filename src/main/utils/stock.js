1848fe954bcbb606406d8e0e64f9f27fd6d44b87
qc5111 <247731705@qq.com>
2019-12-17 16:38:46 +0800

    Update stock.js
    增加指数支持

--- src/main/utils/stock.js
+++ src/main/utils/stock.js
@@ -23,13 +23,22 @@ export default {
             .then(function (results) {
                 for (let index = 0; index < results.length; index++) {
                     var arr = results[index].toString().split(",")
-                    var yesterday_price = parseFloat(arr[2]);
-                    var curr_price = parseFloat(arr[3]);
-                    var percentage = (curr_price - yesterday_price) / yesterday_price * 100
-                    var text = curr_price.toFixed(2) + "," + percentage.toFixed(2) + "%";
+                    console.log(arr.length);
+                    if(arr.length==34){//个股
+                        var yesterday_price = parseFloat(arr[2]);
+                        var curr_price = parseFloat(arr[3]);
+                        var percentage = (curr_price - yesterday_price) / yesterday_price * 100
+                        var text = curr_price + "," + percentage.toFixed(2) + "%";
+                    }else if(arr.length==6){//指数
+                        var curr_price = parseFloat(arr[1]);
+                        var percentage = parseFloat(arr[3]);
+                        var text = curr_price + "," + percentage + "%";
+                    }
+
                     textAll = textAll + text + "||";
                 }
                 textAll = textAll.substring(0, textAll.length - 2)
+                console.log(textAll);
                 callback(textAll)
             })
             .catch(function (e) {
